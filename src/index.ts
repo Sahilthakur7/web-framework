53480eb32e5a80848d021625b34c75c51bab8421
sahilthakur7 <sahilrd07@gmail.com>
2021-01-08 18:11:08 +0530

    User model and functionalities written

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const u1 = new User({ age: 24, name: "kv" });
+console.log(u1.get("age"));
+
+u1.set({ name: "amit" });
+
+console.log(u1.get("age"));
